68078fda2d014d2108d893f3881deec48bb888c6
gizmoanboo-glitch <gizmoanboo@gmail.com>
2025-11-13 13:48:00 -0500

    searches,sorting, and pathfinding

---
+++ backend/backend/algorithms/sorting.py
@@ -0,0 +1,17 @@
+def BubbleSort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+
+
+def QuickSort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return QuickSort(left) + middle + QuickSort(right)
