7e3e0dc90597e1701e877b180f6049c099b27b85
gizmoanboo-glitch <gizmoanboo@gmail.com>
2025-11-13 13:38:38 -0500

    Update app.py

--- backend/backend/app.py
+++ backend/backend/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
-from visualizer.AlgorithmicVisualizer import AlgorithmicVisualizer
+from visualizer.AlgorithmicVisualizer import AlgorithmVisualizer
 from algorithms.sorting import BubbleSort, QuickSort
 from algorithms.searching import LinearSearch, BinarySearch
 from algorithms.pathfinding import BFS, DFS
