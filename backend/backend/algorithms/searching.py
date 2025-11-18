120696e2c913e58ee252a6ac586c117de4207f40
jacobbeeman <jlbeeman0@frostburg.edu>
2025-11-18 13:49:13 -0500

    hi
    hahahahaha

--- backend/backend/algorithms/searching.py
+++ backend/backend/algorithms/searching.py
@@ -4,9 +4,27 @@ class LinearSearch:
 
     def run(self, arr, target):
         for i in range(len(arr)):
+            # Yield each inspection step
+            yield {
+                "array": arr[:],
+                "index": i,
+                "value": arr[i],
+                "target": target,
+                "found": arr[i] == target
+            }
+
             if arr[i] == target:
-                return i
-        return -1
+                return
+
+        # Final step if not found
+        yield {
+            "array": arr[:],
+            "index": None,
+            "value": None,
+            "target": target,
+            "found": False
+        }
+
 
 
 class BinarySearch:
@@ -15,12 +33,37 @@ class BinarySearch:
 
     def run(self, arr, target):
         left, right = 0, len(arr) - 1
+        arr_copy = arr[:]  # prevent mutation
+
         while left <= right:
             mid = (left + right) // 2
-            if arr[mid] == target:
-                return mid
-            elif arr[mid] < target:
+
+            # yield visualizer step
+            yield {
+                "array": arr_copy,
+                "left": left,
+                "mid": mid,
+                "right": right,
+                "value_at_mid": arr_copy[mid],
+                "target": target,
+                "found": arr_copy[mid] == target
+            }
+
+            if arr_copy[mid] == target:
+                return
+
+            elif arr_copy[mid] < target:
                 left = mid + 1
             else:
                 right = mid - 1
-        return -1
+
+        # not found
+        yield {
+            "array": arr_copy,
+            "left": left,
+            "mid": None,
+            "right": right,
+            "value_at_mid": None,
+            "target": target,
+            "found": False
+        }
