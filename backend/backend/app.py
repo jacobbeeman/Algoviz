9ce072e1923d264d65904a17514d71424f55af5e
gizmoanboo-glitch <gizmoanboo@gmail.com>
2025-11-18 12:49:05 -0500

    Update app.py

--- backend/backend/app.py
+++ backend/backend/app.py
@@ -54,11 +54,12 @@ def run_algorithm():
         "steps": steps
     })
 
+@app.route("/animation")
+def animation():
+    return render_template("Frontend_Animation.html")
+
 if __name__ == "__main__":
     # Use 0.0.0.0 for Render deployment
     app.run(host="0.0.0.0", port=5000, debug=True)
 
 
-@app.route("/animation")
-def animation():
-    return render_template("Frontend_Animation.html")
